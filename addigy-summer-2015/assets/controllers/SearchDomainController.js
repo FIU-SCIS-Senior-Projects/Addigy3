2cffa71f02b3ff35676a7b16db50dfff3f5a57f1
Ayme <amorr037@fiu.edu>
2015-06-16 13:59:51 -0400

    labels set depending on elapsed time

--- addigy-summer-2015/assets/controllers/SearchDomainController.js
+++ addigy-summer-2015/assets/controllers/SearchDomainController.js
@@ -1,9 +1,6 @@
 /**
  * Created by ayme on 6/15/15.
  */
-/**
- * Created by ayme on 5/27/15.
- */
 (function () {
     angular.module('app').controller('SearchDomainController', ['DataRequest', '$timeout','$scope', function(DataRequest, $timeout, $scope) {
         var self = this;
@@ -16,6 +13,8 @@
         self.selectedDomain="All";
         self.selectedTopNum=1;
         self.selectedUser="All";
+        self.months=['Jan','Feb','March','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
+        self.labels=[];
 
         function getAllDomains(){
             DataRequest.getAllDomains().
@@ -30,13 +29,12 @@
         getAllDomains();
 
         function updateGraph(){
+            populateGraphScale();
             var topQty=(self.selectedDomain==='All')?self.selectedTopNum:0;
             var fromDate = new Date(self.startDate);
             fromDate = fromDate.setHours(0,0,0,0);
-            //fromDate=fromDate/1000;
             var toDate = new Date(self.endDate);
             toDate = toDate.setHours(23,59,0,0);
-            //toDate = toDate/1000;
             DataRequest.getDomainInfo(self.selectedDomain, self.selectedUser,
                 topQty, fromDate, toDate)
                 .success(function(data, status, headers, config) {
@@ -100,5 +98,71 @@
             'year-format': "'yy'",
             'starting-day': 1
         };
+        Date.prototype.addDays = function(days) {
+            var dat = new Date(this.valueOf())
+            dat.setDate(dat.getDate() + days);
+            return dat;
+        }
+        function populateGraphScale(){
+            var elapsedTime = self.endDate.getTime()-self.startDate.getTime();
+            var elapsedDays = elapsedTime / (1000*60*60*24);
+            if(elapsedDays<2) setScaleToHours();
+            else if(elapsedDays>=2 && elapsedDays<14) setScaleToDays();
+            else if(elapsedDays>=14 && elapsedDays<31) setScaleToWeeks();
+            else setScaleToMonths();
+        }
+        function setScaleToHours(){
+            self.labels.splice(0, self.labels.length);
+            var stopDate=0;
+            if(isDateToday(new Date(self.startDate)))
+                stopDate=new Date().getHours();
+            else stopDate=23;
+            var i;
+            for(i=0;i<=stopDate;i++){
+                self.labels.push(i);
+            }
+            console.log(self.labels);
+        }
+        function setScaleToDays(){
+            self.labels.splice(0,self.labels.length);
+            var currentDate = new Date(self.startDate);
+            while (currentDate <= self.endDate) {
+                self.labels.push(currentDate.toDateString());
+                currentDate = currentDate.addDays(1);
+            }
+            console.log(self.labels);
+        }
+        function setScaleToWeeks(){
+            self.labels.splice(0,self.labels.length);
+            var currentDate = new Date(self.startDate);
+            while (currentDate <= self.endDate) {
+                self.labels.push(currentDate.toDateString()+"-"+currentDate.addDays(7).toDateString())
+                currentDate = currentDate.addDays(8);
+            }
+            console.log(self.labels);
+        }
+        function setScaleToMonths(){
+            self.labels.splice(0,self.labels.length);
+            var currMonth=self.startDate.getMonth();
+            var endMonth = self.endDate.getMonth();
+            while (currMonth <= endMonth) {
+                self.labels.push(self.months[currMonth]);
+                currMonth++;
+            }
+            console.log(self.labels);
+        }
+        function isDateToday(inputDate){
+            var todaysDate = new Date();
+            return (inputDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0));
+
+        }
+        //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        //$scope.series = ['Series A', 'Series B', 'Series C'];
+        //$scope.data = [
+        //    [65, 59, 80, 81, 56, 55, 40],
+        //    [30, 45, 58, 75, 56, 53, 26],
+        //    [28, 48, 40, 19, 86, 27, 90]
+        //];
+
     }]);
 })();
