45fea3a883d84417b101f3edecf336e8c676bc7b
Ayme <amorr037@fiu.edu>
2015-06-17 12:54:08 -0400

    filering and displaying domains info

--- addigy-summer-2015/assets/controllers/SearchDomainController.js
+++ addigy-summer-2015/assets/controllers/SearchDomainController.js
@@ -6,18 +6,16 @@
         var self = this;
         self.allDomains=[];
         self.allUsers=[];
-        self.domainVisitsCount=0;
-        self.domainUsers=[];
         self.domainsFiltered=[];
-        self.domainSelected=false;
-        self.topSelectData=[2,3,4,5,10,20];
+        self.topSelectData=[1,2,3,4,5,6,7,8,9,10];
         self.selectedDomain="All";
         self.selectedTopNum=1;
         self.selectedUser="All";
-        self.daysLabels={'0':'', '1':'', '2':'', '3':'', '4':'', '5':'', '6':'', '7':'', '8':'', '9':'', '10':'',
-            '11':'', '12':'', '13':'', '14':'', '15':'', '16':'', '17':'', '18':'', '19':'', '20':'', '21':'', '22':'',
-            '23':'', '24':'', '25':'', '26':'', '27':'', '28':'', '29':'', '30':'', '31':''};
+        self.daysLabels={};
+        self.visitsPerDayPerDomain={};
         self.labels=[];
+        self.series=[];
+        self.graphData=[];
         self.startDate=0;
         self.endDate=0;
         function getAllDomains(){
@@ -37,8 +35,8 @@
             DataRequest.getDomainInfo(self.selectedDomain, self.selectedUser,
                 topQty, self.startDate, self.endDate)
                 .success(function(data, status, headers, config) {
-                    self.domainsFiltered=data['domainsList'];
-                    console.log(data);
+                    self.domainsFiltered=data['domainList'];
+                    processDomainsData();
             }).error(function(data, status, headers, config) {
                  console.log(data);
             });
@@ -64,36 +62,26 @@
             if (self.selectedDomain==='All') $('#topSelect').prop('disabled', false);
             else $('#topSelect').prop('disabled', 'disabled');
         };
-
-        //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-        //$scope.series = ['Series A', 'Series B', 'Series C'];
-        //$scope.data = [
-        //    [65, 59, 80, 81, 56, 55, 40],
-        //    [30, 45, 58, 75, 56, 53, 26],
-        //    [28, 48, 40, 19, 86, 27, 90]
-        //];
         self.lastMonthSelected=function(){
             var today=new Date();
             var oneMonthAgo=new Date();
             oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
-            today.setHours(23,59,0,0);
-            oneMonthAgo.setHours(0,0,0,0);
-            self.startDate=oneMonthAgo.getTime();
-            self.endDate=today.getTime();
-            populateGraphLabels();
-            updateGraph();
+            dateRangeChanged(today, oneMonthAgo);
         };
         self.lastWeekSelected=function(){
             var today=new Date();
             var oneWeekAgo=new Date();
             oneWeekAgo.setDate(oneWeekAgo.getDate() - 6);
+            dateRangeChanged(today, oneWeekAgo);
+        };
+        function dateRangeChanged(today, pastDate){
             today.setHours(23,59,0,0);
-            oneWeekAgo.setHours(0,0,0,0);
-            self.startDate=oneWeekAgo.getTime();
+            pastDate.setHours(0,0,0,0);
+            self.startDate=pastDate.getTime();
             self.endDate=today.getTime();
             populateGraphLabels();
             updateGraph();
-        };
+        }
         Date.prototype.addDays = function(days) {
             var dat = new Date(this.valueOf())
             dat.setDate(dat.getDate() + days);
@@ -109,7 +97,45 @@
                 start = start.addDays(1);
                 i++;
             }
-            console.log(self.daysLabels);
+        }
+        function processDomainsData(){
+            self.visitsPerDayPerDomain={};
+            var i,j;
+            for(i=0;i<self.domainsFiltered.length;i++){
+                var visitsPerDomain={};
+                var currDomain=self.domainsFiltered[i];
+                var domainName=currDomain['domain'];
+                var visits=currDomain['visits'];
+                for(j=0;j<visits.length;j++){
+                    var visitDate=new Date(visits[j]);
+                    var visitDateStr=visitDate.toDateString();
+                    visitsPerDomain[visitDateStr] ? visitsPerDomain[visitDateStr].value += 1 :
+                        visitsPerDomain[visitDateStr] = {value: 1};
+                }
+                self.visitsPerDayPerDomain[domainName]=visitsPerDomain;
+            }
+            populateGraphData();
+        }
+        function populateGraphData(){
+            self.series.splice(0,self.series.length);
+            self.labels.splice(0,self.labels.length);
+            self.graphData.splice(0,self.graphData.length);
+            var domains=Object.keys(self.visitsPerDayPerDomain);
+            var i,j;
+            for(var key in self.daysLabels) self.labels.push(self.daysLabels[key]);
+            for(i=0;i<domains.length;i++){
+                var domainName=domains[i];
+                self.series.push(domainName);
+                var domainGraphData=[];
+                var domainData=self.visitsPerDayPerDomain[domainName];
+                console.log(domainData)
+                for(j=0;j<self.labels.length;j++){
+                    var visitCount=domainData[self.labels[j]];
+                    if(visitCount==null) domainGraphData.push(0);
+                    else domainGraphData.push(visitCount.value);
+                }
+                self.graphData.push(domainGraphData);
+            }
         }
 
     }]);
