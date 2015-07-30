c1b5adad38a0c466bd9327d60a5ddbc40ae30b29
Matthew Saunders <msaun008@fiu.edu>
2015-07-30 13:44:07 -0400

    Fixing configuration error

--- Code/AdgCollector/scripts/facter.sh
+++ Code/AdgCollector/scripts/facter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export FACTERLIB=/Users/matthewsaunders/repo/Addigy3/AdgCollector/scripts/facter && export LANG=en_US.UTF-8 && /usr/bin/facter -j
+export FACTERLIB=/Users/matthewsaunders/repo/Addigy3/Code/AdgCollector/scripts/facter && export LANG=en_US.UTF-8 && /usr/bin/facter -j
 # export LANG=en_US.UTF-8 && /usr/bin/facter -j
